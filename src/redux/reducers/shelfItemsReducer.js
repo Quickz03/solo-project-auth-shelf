1934735b77c61b5629a99bccf1fbaf64c0a004f7
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-04-18 14:08:31 -0500

    init added reducer definition

---
+++ src/redux/reducers/shelfItemsReducer.js
@@ -0,0 +1,14 @@
+import getShelfItems from '../sagas/getShelfItems';
+
+const shelfItems = (state = [], action) => {
+  switch (action.type) {
+    case `SET_ITEMS`:
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+// shelfItems will be on the redux state at:
+// state.shelfItems
+export default shelfItems;
